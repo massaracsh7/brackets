32de789bd26d3d99f1b17b56d8d34395b281f3a5
Your Name <you@example.com>
2023-01-05 15:13:51 +0300

    feat: add check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const open = [];
+  const pair = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+       open.push(bracketsConfig[i][0]);
+       pair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+      let topElement = stack[stack.length - 1];
+      if (pair[currentSymbol] === topElement && stack.length !== 0) {
+        stack.pop();
+      } else if (open.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+        }
+        else {
+        return false;
+      }
+    
+
+  }
+  return stack.length === 0;
+
 }
